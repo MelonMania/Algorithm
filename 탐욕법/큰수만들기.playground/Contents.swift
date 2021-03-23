e07694c543d509284713cd792688392d3462fd9f
MelonMania <dnwlsskfn@gmail.com>
2021-03-24 00:53:46 +0900

    프로그래머스 Level2

--- 탐욕법/큰수만들기.playground/Contents.swift
+++ 탐욕법/큰수만들기.playground/Contents.swift
@@ -1,3 +1,52 @@
 import UIKit
 
-var str = "Hello, playground"
+//func combination(_ total : Int, _ cnt : Int ) -> Int {
+//    var calcUp : Int = 1
+//    var calcDown : Int = 1
+//    var totalR = total
+//    var cntR = cnt
+//
+//    for _ in 0 ... cnt - 1 {
+//        calcUp *= totalR
+//        totalR -= 1
+//    }
+//
+//    for _ in 0 ... cnt - 1 {
+//        calcDown *= cntR
+//        cntR -= 1
+//    }
+//
+//    return calcUp / calcDown
+//}
+
+
+func solution(_ number: String, _ k:Int) -> String {
+    var numbers: [Character] = number.map { $0 }
+    var count: Int = 0
+    var noMore: Bool = true
+    
+    while count < k {
+        noMore = true
+        for i in 0..<numbers.count-1 {
+            if numbers[i] < numbers[i+1] { // 앞 숫자가 뒷 숫자보다 작다면 삭제
+                numbers.remove(at:i)
+                count += 1
+                noMore = false
+                break
+            }
+        }
+        
+        if noMore {
+            for _ in 0..<k-count {
+                numbers.removeLast()
+            }
+            break
+        }
+    }
+    let answer = numbers.reduce(""){"\($0)\($1)"}
+    return answer
+}
+
+print(solution("1231234", 3))
+
+

00880bed59a88b95a78961db9faab3e50b823427
MelonMania <dnwlsskfn@gmail.com>
2021-05-17 21:46:52 +0900

    HW#3

--- exerciseCode/HW3.4/HW3.4/main.swift
+++ exerciseCode/HW3.4/HW3.4/main.swift
@@ -14,17 +14,16 @@ func solution(_ arr : [Int]) {
     dp[0] = arr[0]
     
     for i in 1 ... arr.count - 1 {
-        if dp[i-1] + arr[i] > arr[i] { // 
+        if dp[i-1] + arr[i] > arr[i] { // 기존의 합과 현재 인덱스의 값과 비교
             dp[i] = dp[i-1] + arr[i]
         }
         else {
             dp[i] = arr[i]
         }
     }
-    print(dp)
-    
+
     for i in 0 ... dp.count - 1{
-        result = max(dp[i], result)
+        result = max(dp[i], result) // 합들중 가장 큰 값 추출
     }
     
     print(result)

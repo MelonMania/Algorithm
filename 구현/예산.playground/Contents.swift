12d07ceae68506e37c413c5b606d703033f9a39f
MelonMania <dnwlsskfn@gmail.com>
2021-06-22 19:25:07 +0900

    프로그래머스 Level1

--- 구현/예산.playground/Contents.swift
+++ 구현/예산.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ d:[Int], _ budget:Int) -> Int {
+ 
+    var index : Int = 0
+    var sum : Int = 0
+    let sorted = d.sorted()
+    
+    while sum < budget {
+        sum += sorted[index]
+        index += 1
+        if sum > budget {
+            index -= 1
+        }
+        
+        if index > sorted.count - 1 {
+            break
+        }
+    }
+    
+    return index
+}
+print(solution([1,3,2,5,4], 9))

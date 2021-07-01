997a7e4c30d607a142029cd2826d7b0c34e34c6f
MelonMania <dnwlsskfn@gmail.com>
2021-07-01 10:36:16 +0900

    BOJ#9093 단어 뒤집기

--- 자료구조/BOJ#9093/BOJ#9093/main.swift
+++ 자료구조/BOJ#9093/BOJ#9093/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+func reverseSentence(_ sentence : String) -> String {
+    let sliceArr = sentence.split(separator: " ").map { $0 }
+    var updatedSen : String = ""
+    for slice in sliceArr {
+        updatedSen += slice.reversed()
+        updatedSen += " "
+    }
+    
+    return updatedSen
+}
+
+let testCase = Int(readLine()!)!
+
+var sentenceList : [String] = []
+
+for _ in 0 ..< testCase {
+    let sentence = readLine()!
+    sentenceList.append(sentence)
+}
+
+for i in 0 ..< sentenceList.count {
+    let updatedSen = reverseSentence(sentenceList[i])
+    print(updatedSen)
+}
+
+
+
+
 
